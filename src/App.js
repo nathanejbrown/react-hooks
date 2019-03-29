3c11b3583e359bfada70f818be7be315c8976f03
Heathcliff <dennis.nathan@gmail.com>
2019-03-29 11:30:16 -0600

    Added useContext and createContext to fake auth.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Todo from './Components/Todo';
+import Header from './Components/Header';
+import Auth from './Components/Auth';
+import AuthContext from './auth-context';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Todo />
-      </div>
-    );
+const app = props => {
+
+  const [page, setPage] = useState('auth');
+  const [authStatus, setAuthStatus] = useState(false);
+
+  const switchPage = pageName => {
+    setPage(pageName);
   }
-}
 
-export default App;
+  const login = () => {
+    setAuthStatus(true);
+  }
+
+  return (
+    <div className="App">
+      <AuthContext.Provider value={{status: authStatus, login: login}}>
+        <Header 
+          onLoadTodos={switchPage.bind(this, 'todos')} 
+          onLoadAuth={switchPage.bind(this, 'auth')} 
+        />
+        <hr />
+        {page === 'auth' ? <Auth /> : <Todo />}
+      </AuthContext.Provider>
+    </div>
+  )
+};
+
+
+export default app;
---
+++ src/Components/Auth.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+
+import AuthContext from '../auth-context';
+
+const auth = props => {
+    const auth = useContext(AuthContext);
+
+    return (
+        <button onClick={auth.login}>Log In!</button>
+    )
+}
+
+
+export default auth;
